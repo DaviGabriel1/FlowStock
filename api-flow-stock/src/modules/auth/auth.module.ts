1a117fe830d158653d2a44237c44ac241bcc83ad
DaviGabriel1 <davig41441503@gmail.com>
2025-03-20 11:48:17 -0300

    feat: middleware de validação de senha

--- api-flow-stock/src/modules/auth/auth.module.ts
+++ api-flow-stock/src/modules/auth/auth.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtService } from '@nestjs/jwt';
 import { MailModule } from '../mail/mail.module';
 import { SessionModule } from '../session/session.module';
+import { ValidateRegisterPasswordMiddleware } from './middlewares/validate-register-password.middleware';
 
 @Module({
   imports: [UsersModule, MailModule, SessionModule],
@@ -12,4 +13,12 @@ import { SessionModule } from '../session/session.module';
   providers: [AuthService, JwtService],
   exports: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ValidateRegisterPasswordMiddleware)
+      .forRoutes({ path: 'auth/email/register', method: RequestMethod.POST });
+  }
+
+
+}
---
+++ api-flow-stock/src/modules/auth/middlewares/validate-register-password.middleware.ts
@@ -0,0 +1,49 @@
+import { Length } from './../../../../../react-flow-stock/node_modules/lightningcss/node/ast.d';
+import { HttpStatus, Injectable, NestMiddleware, UnprocessableEntityException } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
+
+@Injectable()
+export class ValidateRegisterPasswordMiddleware implements NestMiddleware{
+    use(req: Request, res: Response, next: NextFunction) {
+        const { password, repeatPassword } = req.body;
+
+        const hasUppercase = /[A-Z]/;
+        const hasLowercase = /[a-z]/;
+        const hasNumber = /\d/;
+        const hasSpecialChar = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/;
+
+        let errorMessage: string | null = null;
+        
+        if (!password) {
+            errorMessage = 'password must be filled';
+        }
+        else if (!repeatPassword) {
+            errorMessage = 'repeatPassword must be filled';
+        }
+        else if (password.length < 8) {
+            errorMessage = 'password must be 8 characters long';
+        }    
+        else if (password !== repeatPassword) {
+          errorMessage = 'password must be the same';
+        } else if (!hasUppercase.test(password)) {
+          errorMessage = 'the password must have a capital letter';
+        } else if (!hasLowercase.test(password)) {
+          errorMessage = 'the password must have a lowercase letter';
+        } else if (!hasNumber.test(password)) {
+          errorMessage = 'the password must have a number';
+        } else if (!hasSpecialChar.test(password)) {
+          errorMessage = 'the password must have a special character';
+        }
+
+        if (errorMessage) {
+            throw new UnprocessableEntityException({
+              status: HttpStatus.UNPROCESSABLE_ENTITY,
+              errors: {
+                password: errorMessage,
+              },
+            });
+        }
+        next();
+    }
+    
+}
